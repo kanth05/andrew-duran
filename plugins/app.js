955e7a8be7c7dcef280933fe268bb598bce3dd51
kanth05 <azure_05@hotmail.com>
2022-07-02 21:20:50 -0400

    mejora SEO

--- plugins/app.js
+++ plugins/app.js
@@ -1,7 +1,25 @@
-//Variables para manejar fechas en el CV
-let year       = document.getElementById('year');
-let curretYear = document.getElementById('curretYear');
-let date = new Date();
-//Volver los años de experiencia de manera dinámica
-year.innerHTML       = date.getFullYear() - 2017;
-curretYear.innerHTML = date.getFullYear();
+$(document).ready( function(){
+
+    function eventos(){
+
+        $('.nav-link').on( 'click', function(){
+
+            let href = $(this).attr('href');
+            $("html, body").animate({ scrollTop: $(`${href}`).offset().top - 50}, 500);
+
+        });
+
+    }
+
+    //Variables para manejar fechas en el CV
+    let year       = document.getElementById('year');
+    let curretYear = document.getElementById('curretYear');
+    let date = new Date();
+    //Volver los años de experiencia de manera dinámica
+    year.innerHTML       = date.getFullYear() - 2017;
+    curretYear.innerHTML = date.getFullYear();
+
+    eventos();
+
+})
+
